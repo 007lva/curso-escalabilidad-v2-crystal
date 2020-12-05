450339f730d1490976f9f8163c82d239e64ad371
007lva <lvargas@rankia.com>
2020-12-05 10:26:45 +0100

    Simplify structure

--- sesion-6/spec/spec_helper.cr
+++
@@ -1,2 +0,0 @@
-require "spec"
-require "../src/curso-escalabilidad-v2-crystal"
--- sesion-6/src/curso-escalabilidad-v2-crystal.cr
+++
@@ -1,6 +0,0 @@
-# TODO: Write documentation for `Curso::Escalabilidad::V2::Crystal`
-module Curso::Escalabilidad::V2::Crystal
-  VERSION = "0.1.0"
-
-  # TODO: Put your code here
-end
